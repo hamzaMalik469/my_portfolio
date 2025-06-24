6bffac6fa168cbdf34be53fdb995a3786a008ac1
hamzaMalik469 <hamzarivo7@gmail.com>
2025-06-24 23:53:19 +0500

    Fix: firebase.json public path set to build/web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -51,7 +51,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "cf2ef3d085a2e50c7bd162e2e4b01b91
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "a558744d63eb5087f140b201f3ca01c1",
+"flutter_bootstrap.js": "f3141a33e7b3ff53d3c2778479182981",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
