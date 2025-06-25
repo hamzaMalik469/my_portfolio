f6a478f15d30e6e4429c3ccf85de29c1298103f6
hamzaMalik469 <hamzarivo7@gmail.com>
2025-06-25 22:28:03 +0500

    just restarting

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -58,7 +58,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1ce0273949436e9ffafad0026d03b805
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "59e7e380f79d8dee8fc56897f378b6c3",
+"flutter_bootstrap.js": "af4b4fbd8a2afda0e8cce2935d171a27",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
